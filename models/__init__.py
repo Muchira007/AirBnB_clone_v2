fd873b4c493a1c955d5372178c82bfd360852199
muchira007 <muchirasteve2@gmail.com>
2024-04-17 17:55:50 -0700

    no 7

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
-import os
-from models.engine.file_storage import FileStorage
+"""Instantiates a storage object.
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
+from os import getenv
+
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,80 +21,58 @@ class DBStorage:
 
     def __init__(self):
         """ Initializes DBStorage instance. """
-        try:
-            user = os.getenv("HBNB_MYSQL_USER")
-            pwd = os.getenv("HBNB_MYSQL_PWD")
-            host = os.getenv("HBNB_MYSQL_HOST", "localhost")
-            db = os.getenv("HBNB_MYSQL_DB")
-            self.__engine = create_engine(
-                    f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
-                    pool_pre_ping=True)
-            if os.getenv("HBNB_ENV") == "test":
-                Base.metadata.drop_all(self.__engine)
-            Session = scoped_session(sessionmaker(
-                bind=self.__engine, expire_on_commit=False))
-            self.__session = Session()
-        except Exception as e:
-            print(f"Error initializing DBStorage: {e}")
+        user = os.getenv("HBNB_MYSQL_USER")
+        pwd = os.getenv("HBNB_MYSQL_PWD")
+        host = os.getenv("HBNB_MYSQL_HOST", "localhost")
+        db = os.getenv("HBNB_MYSQL_DB")
+        self.__engine = create_engine(
+                f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
+                pool_pre_ping=True)
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """ Query all objects from the current database session. """
-        try:
-            # If cls is a string, resolve the class using globals()
-            if isinstance(cls, str):
-                cls = globals().get(cls)
+        # If cls is a string, resolve the class using globals()
+        if isinstance(cls, str):
+            try:
+                cls = globals()[cls]
+            except KeyError:
+                pass
 
-            # If cls is None, query all subclasses of Base
-            if cls is None:
-                classes = Base.__subclasses__()
-            else:
-                classes = [cls]
+        # If cls is None, query all subclasses of Base
+        if cls is None:
+            classes = Base.__subclasses__()
+        else:
+            classes = [cls]
 
-            objects = {}
-            for cls in classes:
-                query = self.__session.query(cls)
-                for obj in query:
-                    key = f"{cls.__name__}.{obj.id}"
-                    objects[key] = obj
-            return objects
-        except Exception as e:
-            print(f"Error in all method: {e}")
-            return {}
+        objects = {}
+        for cls in classes:
+            query = self.__session.query(cls)
+            for obj in query:
+                key = f"{cls.__name__}.{obj.id}"
+                objects[key] = obj
+        return objects
 
     def new(self, obj):
         """ Add the object to the current database session. """
-        try:
-            if obj:
-                self.__session.add(obj)
-        except Exception as e:
-            print(f"Error adding object to session: {e}")
+        if obj:
+            self.__session.add(obj)
+            self.__session.commit()
 
     def save(self):
         """ Commit all changes of the current database session. """
-        try:
-            self.__session.commit()
-        except Exception as e:
-            print(f"Error saving session: {e}")
+        self.__session.commit()
 
     def delete(self, obj=None):
         """ Delete obj from the current database session. """
-        try:
-            if obj:
-                self.__session.delete(obj)
-        except Exception as e:
-            print(f"Error deleting object from session: {e}")
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
         """ Create all tables and initialize session from the engine. """
-        try:
-            Base.metadata.create_all(self.__engine)
-        except Exception as e:
-            print(f"Error creating tables: {e}")
-
-        try:
-            self.__session.close()
-            Session = scoped_session(sessionmaker(
-                bind=self.__engine, expire_on_commit=False))
-            self.__session = Session()
-        except Exception as e:
-            print(f"Error reloading session: {e}")
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+                bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
